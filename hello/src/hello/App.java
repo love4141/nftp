50e2c61f3901548c8c35012d5fa6b802050b9304
37797 <37797@DESKTOP-MJANLN5>
2019-08-24 16:41:17 +0800

    实现了

---
+++ hello/src/hello/App.java
@@ -0,0 +1,14 @@
+package hello;
+
+import com.google.gson.Gson;
+
+import okhttp3.OkHttpClient;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		OkHttpClient client;
+		Gson gson;
+	}
+}
